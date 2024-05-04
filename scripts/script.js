a5d5e84c9032884162fd109f1c8b27f83308223e
Matheus Santos <80715832+matheus412@users.noreply.github.com>
2024-05-04 11:48:03 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,19 +3,19 @@
         var navMobile = document.getElementById("nav-mobile");
         var menuIcon = document.getElementById("menu-mobile").getElementsByTagName("img")[0];
         var header = document.querySelector("header");
-        var port = document.querySelector('#portfolio')
+
     
         if (navMobile.style.display === "block") {
             navMobile.style.display = "none";
             menuIcon.src = "imagens/icon-hamburger.svg";
             header.style.height = "64px";
-            port.style.marginTop = "1000px"
+            
             
         } else {
             navMobile.style.display = "block";
             menuIcon.src = "imagens/icon-close.svg";
             header.style.height = "250px";
-            port.style.marginTop = "800px"
+
         }
     }
     
